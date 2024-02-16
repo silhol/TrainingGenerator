62a6870d0ca06d656aec74bcd4756862975bf6b7
Silke Holtmanns <54940191+silhol@users.noreply.github.com>
2024-02-16 10:39:49 +0200

    Update TrainingGenerator.py
    added backup element for the database in case I need later another field

--- TrainingGenerator.py
+++ TrainingGenerator.py
@@ -5,27 +5,38 @@
 #
 # Database structure:
 # Columns:
-# Exercise name - The exercise should have an easy to identify name
-# Description text - should describe the exercise
-# Description link - Link to picture or video
-# Skill level - all, kids, normal, rambo, senior, beginners
-# Type of training - warming up, game, strength, stretching, balance, reaction
-# Body part - legs, upper body, shoulder and arms
-# Equipment needed - 1 soft-ball, several soft-balls, bean bags, pistarit, tennis balls, stretching band
-# Amount of persons - 1,2,3,4,5,6,7,8 or more
-# Not suitable for - knees, back, breast, hip, neck, ankle
-# Duratation - x minutes
-# 
+# 1. Exercise name - The exercise should have an easy to identify name
+# 2. Description text - should describe the exercise
+# 3. Description link - Link to picture or video
+# 4. Skill level - all, kids, normal, rambo, senior, beginners
+# 5. Type of training - warming up, game, strength, stretching, balance, reaction
+# 6. Body part - legs, upper body, shoulder and arms
+# 7. Equipment needed - 1 soft-ball, several soft-balls, bean bags, pistarit, tennis balls, stretching band
+# 8. Amount of persons - 1,2,3,4,5,6,7,8 or more
+# 9. Not suitable for - knees, back, breast, hip, neck, ankle
+# 10. Duration - x minutes
+# 11. Comments - optional free text
+# 12. Backup - just in case I need a field more
 
-# Intended usage:
+# Main UI
+# User selects task (after that he is returned to here):
+# - Do you want to add an exercise?
+# - Do you want to create a training plan?
+# - Modify an existing training plan?
+# - Do you want to view all the exercises in the database?
+# - Do you want to edit/remove an exercise?
+
+# Crate training plan function (after "want to create a training plan")
+# 
 # User choose from dropdown menu:
 # age, type, body part, equipment need, amount of persons, not suitable for
 # This is repeated till he says "enough exercises" (age is cached)
 # Outcome: Training plan, sorting for warming up, main training, in-between games, flexing
 
-# Additional features
-# Adding of exercises
+# Planned functions
+# Adding of new exercises
 # Modification of entries in the database
+# Listing of all exercises in the database in a readable format
 # Modification of received trainings plan
 
 import sqlite3
@@ -47,30 +58,46 @@ def insert_new_exercise(cursor):  # this function is for adding new exercises to
 
     # Create a function to store the sentences in an array
     def store_exercise_elements():
-        element1 = entry1.get()  # Exercise Name
-        element2 = entry2.get()  # Exercise Description
-        element3 = entry3.get()  # Exercise Link
+        element1 = entry1.get()  # 1. Exercise Name
+        element2 = entry2.get()  # 2. Exercise Description
+        element3 = entry3.get()  # 3. Exercise Link
 
-        # Multiple Choice entry by user to be stored for the skill level (formerly categories)
-        selected_skill_level = [skill_level for skill_level, vari in skill_level_vars.items() if vari.get()]
+        # 4. Skill Level - Multiple Choice entry by user to be stored for the skill level (formerly categories)
+        selected_skill_level = [skill_level for skill_level, i in skill_level_details.items() if i.get()]
         element4 = ", ".join(selected_skill_level)  # Join selected skill levels
+        
+        # 5. Type of training - warming up, game, strength, stretching, balance, reaction
+        selected_type_of_training = [training_type for training_type, i in training_type_details.items() if i.get()]
+        element5 = ", ".join(selected_type_of_training)  # Join selected traing types
+        
+        ########CHECK BELOW WHICH GET NORMAL TEXT!!
 
-        ########CHECK BELOW WHICH GET NORMAL TEXT!!!
-
-        element5 = entry5.get()
         element6 = entry6.get()
         element7 = entry7.get()
         element8 = entry8.get()
         element9 = entry9.get()
-        element10 = entry10.get()
+        
+        ##################### 
+        element10 = entry10.get()  # 10. Duration - x minutes
+        element11 = entry11.get()  # Comments
+        element12 = "Backup field"
+        # element12 = entry12.get()  # Backup field can be used if the DB needs one column more
 
         new_exercise.append((element1, element2, element3, element4, element5, element6, element7,
-                             element8, element9, element10))
+                             element8, element9, element10, element11, element12))
 
-        entry1.delete(0, 'end')  # Clear the input fields
-        entry2.delete(0, 'end')
-        for vari in skill_level_vars.values():
-            vari.set(0)  # Reset checkbox values
+        # Cleaning of input fields
+        entry1.delete(0, 'end')  # Clear the input fields for text input 1. Exercise Name
+        entry2.delete(0, 'end')  # Clear the input fields for text input 2. Exercise Description
+        entry3.delete(0, 'end')  # Clear the input fields for text input 3. Exercise Link
+        
+        # Cleaning of input field 4. Skill Level
+        for i in skill_level_details.values():
+            i.set(0)  # Reset checkbox values
+        
+        # Cleaning of input field 5. Training Type
+        for i in training_type_details.values():
+            i.set(0)  # Reset checkbox values
 
         # CLEAN OTHER ELEMENTS
 
@@ -105,7 +132,7 @@ def insert_new_exercise(cursor):  # this function is for adding new exercises to
     label8 = tk.Label(root, text="How many persons are needed at least to perform this exercise (select from menu):")
     label9 = tk.Label(root, text="For what / whom is this exercise NOT suitable (select from menu):")
     label10 = tk.Label(root, text="How long will the exercise take (optional, if you do not know just press return):")
-
+    label11 = tk.Label(root, text="Comments:")
     # Exercise name - The exercise should have an easy to identify name
     # Description text - should describe the exercise
     # Description link - Link to picture or video
@@ -128,7 +155,8 @@ def insert_new_exercise(cursor):  # this function is for adding new exercises to
     entry8 = tk.Entry(root)
     entry9 = tk.Entry(root)
     entry10 = tk.Entry(root)
-
+    entry11 = tk.Entry(root)
+    
     label1.pack()
     entry1.pack()
     label2.pack()
@@ -149,6 +177,8 @@ def insert_new_exercise(cursor):  # this function is for adding new exercises to
     entry9.pack()
     label10.pack()
     entry10.pack()
+    label11pack()
+    entry11pack()
     # Checkboxes for exercise categories
     category_vars = {
         "Games": IntVar(),
